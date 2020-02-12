c677ba08847aeb32c219d065434ffa61444e638a
Khris Punzalan <khrispunzalan@gmail.com>
2020-02-11 16:27:26 -0800

    added validations to models

--- backend-cocktail-api/app/models/cocktail.rb
+++ backend-cocktail-api/app/models/cocktail.rb
@@ -1,4 +1,7 @@
 class Cocktail < ApplicationRecord
     has_many :liquors_cocktails
     has_many :liquors, through: :liquors_cocktails
+
+    validates :name, presence: true
+    validates :name, uniqueness: { case_sensitive: false }
 end
--- backend-cocktail-api/app/models/liquor.rb
+++ backend-cocktail-api/app/models/liquor.rb
@@ -1,4 +1,7 @@
 class Liquor < ApplicationRecord
     has_many :liquors_cocktails
     has_many :cocktails, through: :liquors_cocktails
+
+    validates :name, presence: true
+    validates :name, uniqueness: { case_sensitive: false }
 end
