69f339846d12e41af12720f32f43321e2e9597dd
Khris Punzalan <khrispunzalan@gmail.com>
2020-02-14 05:40:24 -0800

    fetched all cocktails from db

---
+++ frontend-cocktail-api/src/components/cocktail.js
@@ -0,0 +1,10 @@
+class Cocktail {
+    constructor(cocktail) {
+        this.id = cocktail.id
+        this.name = cocktail.name
+        this.flavor = cocktail.flavor
+        this.ingredient = cocktail.ingredient
+        this.preparation = cocktail.preparation
+    }
+}
+
--- frontend-cocktail-api/src/components/cocktails.js
+++ frontend-cocktail-api/src/components/cocktails.js
@@ -7,9 +7,27 @@ class Cocktails {
     }
 
     fetchAndLoadCocktails() {
-        this.adapter.getCocktails().then(cocktails => {
-            console.log(cocktails)
-        })
+        this.adapter
+            .getCocktails()
+            .then(cocktails => {
+                // console.log(cocktails)
+                // for(const cocktail of cocktails.data) {
+                cocktails.data.forEach(cocktail => {
+                    let cocktailObj = {
+                        id: cocktail.id,
+                        name: cocktail.attributes.name,
+                        flavor: cocktail.attributes.flavor,
+                        ingredient: cocktail.attributes.ingredient,
+                        preparation: cocktail.attributes.preparation
+                    }
+                    let newCocktail = new Cocktail(cocktailObj)
+                    this.cocktails.push(newCocktail)
+                    // console.log(this.cocktails)
+                })
+            
+            })
+            console.log(this.cocktails)
+            // .then(() => this.renderCocktails())
     }
 
 }
