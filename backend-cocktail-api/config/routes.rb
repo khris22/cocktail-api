cb0771f7d2e8463d83c36a92b92c69761fae63e6
Khris Punzalan <khrispunzalan@gmail.com>
2020-02-20 18:29:48 -0800

    deleted route for join table

--- backend-cocktail-api/config/routes.rb
+++ backend-cocktail-api/config/routes.rb
@@ -1,15 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  # get '/test', to: 'application#test'
   # root 'cocktails#index'
-  
   namespace :api do
     namespace :v1 do
       resources :cocktails  
       resources :liquors
-      # resources :liquor_cocktails
     end
   end
-
 end
